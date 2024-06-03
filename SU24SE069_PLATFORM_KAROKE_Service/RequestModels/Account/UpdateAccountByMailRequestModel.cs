88ae8210c5165b74f24f050b94e218112a7adf66
nea287 <thonguyenngockieu@gmail.com>
2024-06-03 12:59:47 +0700

    fix: update account model

--- SU24SE069_PLATFORM_KAROKE_Service/RequestModels/Account/UpdateAccountByMailRequestModel.cs
+++ SU24SE069_PLATFORM_KAROKE_Service/RequestModels/Account/UpdateAccountByMailRequestModel.cs
@@ -13,7 +13,7 @@ namespace SU24SE069_PLATFORM_KAROKE_BusinessLayer.RequestModels.Account
         public string AccountName { get; set; } = null!;
         public int Star { get; set; }
         public string? Fullname { get; set; }
-        public string? Yob { get; set; }
+        public int? Yob { get; set; }
         public string? IdentityCardNumber { get; set; }
         public string? PhoneNumber { get; set; }
     }
