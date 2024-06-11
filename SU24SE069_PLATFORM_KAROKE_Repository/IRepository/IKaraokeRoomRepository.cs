043cc7dcaba59e13e22a8bbd4227bc18272d4dff
nea287 <thonguyenngockieu@gmail.com>
2024-06-12 00:03:17 +0700

    create: interface karaokeroom repo

---
+++ SU24SE069_PLATFORM_KAROKE_Repository/IRepository/IKaraokeRoomRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SU24SE069_PLATFORM_KAROKE_Repository.IRepository
+{
+    public interface IKaraokeRoomRepository
+    {
+    }
+}
