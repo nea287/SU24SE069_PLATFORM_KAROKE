2b535b458c730cf8115561f6b912b1abb072cf93
nea287 <thonguyenngockieu@gmail.com>
2024-06-12 00:05:40 +0700

    motify: karaoke room repo

--- SU24SE069_PLATFORM_KAROKE_Repository/IRepository/IKaraokeRoomRepository.cs
+++ SU24SE069_PLATFORM_KAROKE_Repository/IRepository/IKaraokeRoomRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SU24SE069_PLATFORM_KAROKE_DataAccess.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SU24SE069_PLATFORM_KAROKE_Repository.IRepository
 {
-    public interface IKaraokeRoomRepository
+    public interface IKaraokeRoomRepository : IBaseRepository<KaraokeRoom>
     {
     }
 }
