8d4e415637d72c401e87263173221dcf95db6850
nea287 <thonguyenngockieu@gmail.com>
2024-06-18 18:15:23 +0700

    update: song (song code is generation)

--- SU24SE069_PLATFORM_KAROKE_Service/RequestModels/Song/CreateSongRequestModel.cs
+++ SU24SE069_PLATFORM_KAROKE_Service/RequestModels/Song/CreateSongRequestModel.cs
@@ -11,7 +11,7 @@ namespace SU24SE069_PLATFORM_KAROKE_Service.RequestModels.Song
         public string SongName { get; set; } = null!;
         public string? SongDescription { get; set; }
         public string? SongUrl { get; set; }
-        public string? SongCode { get; set; }
+        //public string? SongCode { get; set; }
         public DateTime? PublicDate { get; set; }
         public Guid CreatorId { get; set; }
         public decimal Price { get; set; }
