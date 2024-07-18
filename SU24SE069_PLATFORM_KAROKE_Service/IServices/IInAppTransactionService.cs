02b5e1dac5b7ca8a92dd2194db7f1e226c09941a
nea287 <thonguyenngockieu@gmail.com>
2024-07-18 20:22:37 +0700

    purchase song api

--- SU24SE069_PLATFORM_KAROKE_Service/IServices/IInAppTransactionService.cs
+++ SU24SE069_PLATFORM_KAROKE_Service/IServices/IInAppTransactionService.cs
@@ -3,6 +3,7 @@ using SU24SE069_PLATFORM_KAROKE_BusinessLayer.ReponseModels.Helpers;
 using SU24SE069_PLATFORM_KAROKE_BusinessLayer.RequestModels.Helpers;
 using SU24SE069_PLATFORM_KAROKE_Service.ReponseModels;
 using SU24SE069_PLATFORM_KAROKE_Service.RequestModels.InAppTransaction;
+using SU24SE069_PLATFORM_KAROKE_Service.RequestModels.PurchasedSong;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,8 @@ namespace SU24SE069_PLATFORM_KAROKE_Service.IServices
         public Task<ResponseResult<InAppTransactionViewModel>> CreateInAppTransaction(CrreateInAppTransactionRequestModel request);
         public Task<ResponseResult<InAppTransactionViewModel>> UpdateInAppTransaction(UpdateInAppTransactionRequestModel request, Guid id);
         public Task<DynamicModelResponse.DynamicModelsResponse<InAppTransactionViewModel>> GetTransactions(InAppTransactionViewModel filter, PagingRequest paging, InAppTransactionOrderFilter orderFilter);
+
+        public Task<ResponseResult<InAppTransactionViewModel>> PurchaseSong(PurchasedSongRequestModel request);
 
 
     }
