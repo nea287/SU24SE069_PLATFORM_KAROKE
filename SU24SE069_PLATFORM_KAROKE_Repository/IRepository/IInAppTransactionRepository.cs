820cfc788b259d79962f9d6cd71216f158e7bc70
nea287 <thonguyenngockieu@gmail.com>
2024-07-19 16:24:29 +0700

    fix: api purchase item

--- SU24SE069_PLATFORM_KAROKE_Repository/IRepository/IInAppTransactionRepository.cs
+++ SU24SE069_PLATFORM_KAROKE_Repository/IRepository/IInAppTransactionRepository.cs
@@ -13,6 +13,8 @@ namespace SU24SE069_PLATFORM_KAROKE_Repository.IRepository
         //public bool DeleteAccountInventory(AccountInventoryItem request);
         public Task<bool> UpdateInAppTransaction(Guid id, InAppTransaction request);
         public bool CheckPurchasedSong(Guid memberId, Guid songId);
+        public Task<bool> CheckPurchasedSong1(Guid memberId, Guid songId);
         public bool CheckPurchasedItem(Guid memberId, Guid itemId);
+        public Task<bool> CheckPurchasedItem1(Guid memberId, Guid itemId);
     }
 }
--- SU24SE069_PLATFORM_KAROKE_Service/RequestModels/Item/PurchaseItemRequestModel.cs
+++ SU24SE069_PLATFORM_KAROKE_Service/RequestModels/Item/PurchaseItemRequestModel.cs
@@ -9,5 +9,7 @@ namespace SU24SE069_PLATFORM_KAROKE_Service.RequestModels.Item
         public Guid MemberId { get; set; }
         [Required(ErrorMessage = Constraints.EMPTY_INPUT_INFORMATION)]
         public Guid ItemId { get; set; }
+        [Required(ErrorMessage = Constraints.EMPTY_INPUT_INFORMATION)]
+        public int Quantity { get; set; }
     }
 }
