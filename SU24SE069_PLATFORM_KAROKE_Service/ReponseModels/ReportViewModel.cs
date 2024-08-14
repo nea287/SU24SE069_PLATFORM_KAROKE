e954627d2087dc4f6ed6ac2e75810e1e1959c5dd
nea287 <thonguyenngockieu@gmail.com>
2024-08-15 02:48:02 +0700

    update db

--- SU24SE069_PLATFORM_KAROKE_Service/ReponseModels/ReportViewModel.cs
+++ SU24SE069_PLATFORM_KAROKE_Service/ReponseModels/ReportViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SU24SE069_PLATFORM_KAROKE_BusinessLayer.Commons;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,11 @@ namespace SU24SE069_PLATFORM_KAROKE_Service.ReponseModels
         public Guid? ReportId { get; set; }
         public Guid? ReporterId { get; set; }
         public Guid? ReportedAccountId { get; set; }
-        public int? ReportCategory { get; set; }
-        public int? Status { get; set; }
+        public ReportCatagory? ReportCategory { get; set; }
+        public ReportStatus? Status { get; set; }
         public string? Reason { get; set; }
         public DateTime? CreateTime { get; set; }
-        public int? ReportType { get; set; }
+        public ReportType? ReportType { get; set; }
         public Guid? CommentId { get; set; }
         public Guid? PostId { get; set; }
         public Guid? RoomId { get; set; }
